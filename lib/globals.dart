913317e0d01e040f425d042956e0a6d17497fc88
Andre Barquet <andreluiz97.bm@gmail.com>
2022-06-01 09:57:35 -0300

    first commit

---
+++ lib/globals.dart
@@ -0,0 +1,5 @@
+library aguage.globals;
+import 'package:flutter/material.dart';
+
+const Color mainColor = Color.fromARGB(255, 100, 66, 15);
+const Color textColor = Color.fromARGB(255, 202, 219, 118);
